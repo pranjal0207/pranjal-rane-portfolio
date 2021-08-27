7ec6b8d7bc74101f7dbb66bc834b603035453bad
Pranjal Rane <rane.pranjal18@vit.edu>
2021-08-27 22:22:44 +0530

    project card optimised

--- lib/widgets/projectCard.dart
+++ lib/widgets/projectCard.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
+import 'dart:js' as js;
+
+final double maxWidth = 1200;
 
 class ProjectCard extends StatelessWidget {
   final String title;
   final String description;
   final String image;
+  final String githubRepo;
+  final bool paperAvailable;
+  final String paperLink;
+
   const ProjectCard({ 
     required this.title,
     required this.description,
     required this.image,
+    required this.githubRepo,
+    required this.paperAvailable,
+    required this.paperLink,
     Key? key
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final double maxWidth = 1400;
     return PhysicalModel(
       color: Colors.white,
       elevation: 4,
@@ -22,33 +31,147 @@ class ProjectCard extends StatelessWidget {
       child: Container(
         width: (maxWidth - 80) /3,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Container(
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(10),
-                  topRight: Radius.circular(10),
-                ),
-                child: Image.asset(
-                  image,
-                  fit: BoxFit.cover,
-                ),
-              )
+            ProjectImage(
+              image: image
             ),
 
-            Container(
-              child: SelectableText(
-                title
-              ),
-            ),
+            ProjectTitle(
+              title: title, 
+              githubRepo: githubRepo,
+              paperAvailable: paperAvailable,
+              paperLink: paperLink,
+            ),            
 
-            Container(
-              child: SelectableText(
-                description
-              ),
-            ),
+            ProjectDescription(
+              description: description
+            )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ProjectImage extends StatelessWidget {
+  final String image;
+  const ProjectImage({ 
+    required this.image,
+    Key? key 
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(10),
+          topRight: Radius.circular(10),
+        ),
+        child: Image.asset(
+          image,
+          height: 200,
+          width: (maxWidth - 80) /3,
+          fit: BoxFit.fill,
+        ),
+      )
+    );
+  }
+}
+
+class ProjectTitle extends StatelessWidget {
+  final String title;
+  final String githubRepo;
+  final bool paperAvailable;
+  final String paperLink;
+
+  const ProjectTitle({ 
+    required this.title,
+    required this.githubRepo,
+    required this.paperAvailable,
+    required this.paperLink,
+    Key? key 
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 10, left: 10, right: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+            width: ((maxWidth - 80) / 3) * 0.73,
+            child: SelectableText(
+              title,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+
+          Container(
+            child: Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  if(paperAvailable)
+                  IconButton(
+                    padding: EdgeInsets.zero,
+                    onPressed: (){
+                      js.context.callMethod('open', [paperLink]);
+                    }, 
+                    icon: Image.asset("assets/icons/pdfIcon.png"),
+                    splashColor: Colors.transparent,
+                    splashRadius: 0.1,
+                    tooltip : "View Research Paper"
+                  ),
+
+                  IconButton(
+                    onPressed: (){
+                      js.context.callMethod('open', [githubRepo]);
+                    }, 
+                    icon: Image.asset("assets/icons/githubIcon.png"),
+                    splashColor: Colors.transparent,
+                    splashRadius: 0.1,
+                    tooltip : "View Code Repository"
+                  ),
+                ],
+              ),
+            )
+          )
+        ],
+      )
+    );
+  }
+}
+
+class ProjectDescription extends StatelessWidget {
+  final String description;
+  const ProjectDescription({ 
+    required this.description,
+    Key? key 
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      margin: EdgeInsets.only(top: 15, left: 10, right: 10),
+      child: Column(
+        children : <Widget>[
+          Container(
+            child: SelectableText(
+              description,
+              textAlign: TextAlign.justify,
+              style: TextStyle(
+                fontSize: 17
+              ),
+            ),
+          )
+        ]
       ),
     );
   }
--- lib/widgets/projectsPage.dart
+++ lib/widgets/projectsPage.dart
@@ -6,29 +6,38 @@ class ProjectPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final double maxWidth = 1400;
+    final double maxWidth = 1200;
     return Container(
       constraints: BoxConstraints(maxWidth: maxWidth),
-      margin: EdgeInsets.only(left: 40, right: 40, top: 40, bottom: 40),
+      margin: EdgeInsets.only(top: 40, bottom: 40),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           ProjectCard(
-            title: "Project Name", 
-            description: "Project Description", 
-            image: "assets/projectImages/projectStock.jpg"
+            title: "Web Based System for Face Mask Detection & Face Recognition", 
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius nec felis quis interdum. Duis vel metus sed lacus iaculis condimentum id ut ante. Praesent gravida mauris vitae ante egestas, eget vestibulum mauris pulvinar. Quisque tellus elit, mollis et enim vitae, lobortis scelerisque tortor. Etiam non arcu sem. Vivamus consectetur.", 
+            image: "assets/projectImages/projectStock.jpg",
+            githubRepo: "https://github.com/pranjal0207", 
+            paperAvailable: true, 
+            paperLink: "https://www.ijariit.com/manuscripts/v7i4/V7I4-1684.pdf",
           ),
 
           ProjectCard(
             title: "Project Name", 
             description: "Project Description", 
-            image: "assets/projectImages/projectStock.jpg"
+            image: "assets/projectImages/projectStock.jpg",
+            githubRepo: "https://github.com/pranjal0207",
+            paperAvailable: false, 
+            paperLink: "",
           ),
 
           ProjectCard(
             title: "Project Name", 
             description: "Project Description", 
-            image: "assets/projectImages/projectStock.jpg"
+            image: "assets/projectImages/projectStock.jpg",
+            githubRepo: "https://github.com/pranjal0207",
+            paperAvailable: false, 
+            paperLink: "",
           ),
         ],
       )
